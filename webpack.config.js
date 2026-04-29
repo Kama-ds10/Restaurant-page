643c84469365bd2059d136b90652c8f7267c326d
kama_ds10 <kamazd1234@gmail.com>
2026-04-29 10:46:11 +0100

    build: configure webpack
    - Added webpack configuration
    - Setup HtmlWebpackPlugin
    - Added start and build scripts

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    port: 8080,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+};
